40efbe11fa08a7831b16f22431acd2080cff8868
Maple <ssh1987@sina.com>
2020-01-02 17:05:42 +0800

    更新简洁代码

--- RotateArray_upgrade.java
+++ RotateArray_upgrade.java
@@ -1,8 +1,10 @@
 class RotateArray_upgrade {
 	public void rotate(int[] nums, int k) {
+		k %= num.length;
+
 		nums = reverseArrays(nums, 0, nums.length - 1);
-		nums = reverseArrays(nums, 0, k % nums.length - 1);
-		nums = reverseArrays(nums, k % nums.length, nums.length - 1);
+		nums = reverseArrays(nums, 0, k - 1);
+		nums = reverseArrays(nums, k, nums.length - 1);
 	}
 
 	private int[] reverseArrays(int[] nums, int begin, int end) {
