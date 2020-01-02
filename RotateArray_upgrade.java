7284d0fb431c73d44a1f1017b70675e95577d1f6
Maple <ssh1987@sina.com>
2020-01-02 17:04:05 +0800

    RotateArray升级算法版

---
+++ RotateArray_upgrade.java
@@ -0,0 +1,18 @@
+class RotateArray_upgrade {
+	public void rotate(int[] nums, int k) {
+		nums = reverseArrays(nums, 0, nums.length - 1);
+		nums = reverseArrays(nums, 0, k % nums.length - 1);
+		nums = reverseArrays(nums, k % nums.length, nums.length - 1);
+	}
+
+	private int[] reverseArrays(int[] nums, int begin, int end) {
+		while (begin < end) {
+			int temp = nums[begin];
+			nums[begin] = nums[end];
+			nums[end] = temp;
+			begin++;
+			end--;
+		}
+		return nums;
+	}
+}
